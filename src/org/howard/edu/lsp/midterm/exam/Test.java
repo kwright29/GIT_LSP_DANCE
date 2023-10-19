ac09b292633c801edf947ec692b4669e77ba01ef
kwright29 <khloewright329@gmail.com>
2023-10-19 15:43:49 -0400

    adding test.java

---
+++ src/org/howard/edu/lsp/midterm/exam/Test.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.exam;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("This class is just so I can upload my exam to github within this package.");
+
+	}
+
+}
