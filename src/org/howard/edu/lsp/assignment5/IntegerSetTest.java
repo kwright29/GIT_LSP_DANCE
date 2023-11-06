fe670323a4d6c8fe529b3602dbf0c0c947e33fee
kwright29 <khloewright329@gmail.com>
2023-11-06 11:59:16 -0500

    adding rest of passing unit tests

--- src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -14,6 +14,8 @@ public class IntegerSetTest {
 	private IntegerSet set0;
 	private IntegerSet set1;
 	private IntegerSet set2;
+	private IntegerSet set3;
+	private IntegerSet set4;
 	
 	@BeforeEach
 	void setUp() {
@@ -28,6 +30,16 @@ public class IntegerSetTest {
 		set2.add(3);
 		set2.add(6);
 		set2.add(9);
+		
+		set3 = new IntegerSet();
+		set3.add(4);
+		set3.add(8);
+		set3.add(12);
+		
+		set4 = new IntegerSet();
+		set4.add(4);
+		set4.add(8);
+		set4.add(12);
 	
 	}
 	
@@ -63,6 +75,7 @@ public class IntegerSetTest {
 	@Test
 	@DisplayName ("test largest, throws exception")
 	public void testLargest_throwsException() throws IntegerSetException {
+		//getting largest from empty set
 		Throwable exception = assertThrows(IntegerSetException.class, () -> set0.largest());
 		assertNotNull(exception.getMessage());
 		assertEquals("set is empty", exception.getMessage());
@@ -78,6 +91,7 @@ public class IntegerSetTest {
 	@Test
 	@DisplayName ("test smallest, throws exception")
 	public void testSmallest_throwsException() throws IntegerSetException {
+		// getting smallest from empty set
 		Throwable exception = assertThrows(IntegerSetException.class, () -> set0.smallest());
 		assertNotNull(exception.getMessage());
 		assertEquals("set is empty", exception.getMessage());
@@ -86,9 +100,11 @@ public class IntegerSetTest {
 	@Test
 	@DisplayName("test add")
 	public void testAdd() {
+		//adding to empty set
 		set0.add(4);
-		set1.add(4);
 		assertEquals(set0.toString(), "[4]");
+		
+		set1.add(4);
 		assertEquals(set1.toString(), "[1, 2, 3, 4]");
 	}
 	
@@ -98,10 +114,72 @@ public class IntegerSetTest {
 		set2.remove(6);
 		assertEquals(set2.toString(), "[3, 9]");
 		
+		//removing an element that isn't there
 		set1.remove(5);
 		assertEquals(set1.toString(), "[1, 2, 3]");
 	}
 	
+	@Test
+	@DisplayName("test union")
+	public void testUnion() {
+		set1.union(set2);
+		assertEquals(set1.toString(), "[1, 2, 3, 6, 9]");
+		
+		//unionizing with an empty set
+		set0.union(set3);
+		assertEquals(set0.toString(), "[4, 8, 12]");
+	}
+	
+	@Test
+	@DisplayName("test intersection")
+	public void testIntersection() {
+		set1.intersection(set2);
+		assertEquals(set1.toString(), "[3]");
+		
+		//checking two sets that have no alike elements
+		set2.intersection(set3);
+		assertEquals(set2.toString(), "[]");
+	}
+	
+	@Test
+	@DisplayName("test difference")
+	public void testDiff() {
+		set1.diff(set2);
+		assertEquals(set1.toString(), "[1, 2]");
+		
+		// getting difference from two equal sets
+		set3.diff(set4);
+		assertEquals(set3.toString(), "[]");
+	}
+	
+	@Test
+	@DisplayName("test complement")
+	public void testComplement() {
+		set1.complement(set2);
+		assertEquals(set1.toString(), "[6, 9]");
+		
+		//complement of two sets that have no alike elements
+		set2.complement(set3);
+		assertEquals(set2.toString(), "[4, 8, 12]");
+		
+		//complement of two alike sets
+		set3.complement(set4);
+		assertEquals(set3.toString(), "[]");
+	}
+	
+	@Test
+	@DisplayName("test isEmpty")
+	public void testIsEmpty() {
+		assertTrue(set0.isEmpty() == true);
+		assertTrue(set3.isEmpty() == false);
+	}
+	
+	@Test
+	@DisplayName("test toString") 
+	public void testToString() {
+		assertEquals(set0.toString(), "[]");
+		assertEquals(set2.toString(), "[3, 6, 9]");
+	}
 	
 
 }
