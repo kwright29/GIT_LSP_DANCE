7b2283b46cc3ec442b088fa379fb7e4fe12d302c
kwright29 <khloewright329@gmail.com>
2023-12-07 19:06:25 -0500

    adding problem 51 for final

---
+++ src/howard/edu/lsp/exam/problem51/Logger.java
@@ -0,0 +1,26 @@
+package howard.edu.lsp.exam.problem51;
+
+public class Logger {
+	private String log;
+	private static Logger logger;
+	
+	private Logger() {
+		this.log = "";
+	}
+	
+	public void logMessage(String message) {
+		this.log += message + "\n";
+	}
+	
+	public String displayLog() {
+        return("Log:\n" + log);
+    }
+	
+	public static Logger getInstance() {
+		if (logger == null) {
+			logger = new Logger();
+		}
+		return logger;
+	}
+
+}
---
+++ src/howard/edu/lsp/exam/problem51/TestLogger.java
@@ -0,0 +1,39 @@
+package howard.edu.lsp.exam.problem51;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestLogger {
+	private Logger logger;
+	private Logger logger1;
+	private Logger logger2;
+	
+	@BeforeEach
+	void setUp() {
+		logger = Logger.getInstance(); 
+		logger1 = Logger.getInstance();
+		logger2 = Logger.getInstance();
+;	}
+	
+	@Test
+	@DisplayName("test displaylog")
+	public void test_displayLog() {
+		logger.logMessage("Hello, this is a test");
+		assertEquals("Log:\nHello, this is a test\n", logger.displayLog());
+		
+		logger.logMessage("It's a Beautiful Day");
+		assertEquals("Log:\nHello, this is a test\nIt's a Beautiful Day\n", logger.displayLog());
+	}
+	
+	@Test
+	@DisplayName("test log instances")
+	public void test_logger_instances() {
+		assertEquals(logger, logger1);
+		assertEquals(logger1, logger2);
+	}
+
+}
